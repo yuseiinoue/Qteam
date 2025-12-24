6211704f0607ef1f8f68b2a343b574621bbce240
kyogo207 <f24ba025@chuo.ac.jp>
2025-12-24 11:43:25 +0900

    Format event dates as month/day

--- frontend/events.js
+++ frontend/events.js
@@ -38,8 +38,9 @@ function displayEvents(events) {
         eventElement.dataset.area = event.area;
 
         // バグ: 日付をパースせずにそのまま表示している
-        const month = event.event_date;
-        const day = '';
+        const eventDate = new Date(event.event_date);
+        const month = eventDate.getMonth() + 1;
+        const day = eventDate.getDate();
 
         // エリア名を日本語に変換
         const areaNames = {
